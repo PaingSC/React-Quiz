55ca9303b49132605693e5da7c01b5044b6cca28
PaingSC <paingsoechit.job@gmail.com>
2024-02-15 23:13:49 +0630

    Updating:DateCounter.js:Hook:useReducer

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,64 +1,65 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
+
+const initialState = { count: 0, step: 1 };
 
 function reducer(state, action) {
   console.log(state, action);
-  // state += action;
-  // console.log(state);
-  // return state;
+  switch (action.type) {
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      console.log(state);
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
 
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "setCount") return action.payload;
+    default:
+      throw new Error("Unknow action");
+  }
 }
 
 function DateCounter() {
-  // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const [step, setStep] = useState(1);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   // const date = new Date();
   date.setDate(date.getDate() + count);
 
+  // event handlers starts here
   const dec = function () {
-    // dispatch(-1);
     dispatch({ type: "dec" });
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step);
   };
 
   const inc = function () {
-    // dispatch(1);
     dispatch({ type: "inc" });
-
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    // dispatch(Number(e.target.value));
     dispatch({ type: "setCount", payload: Number(e.target.value) });
-
-    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    // setCount(0);
-    setStep(1);
+    dispatch({ type: "reset" });
   };
+  // event handlers ends here
 
   return (
     <div className="counter">
       <div>
         <input
           type="range"
-          min="0"
+          min="1"
           max="10"
           value={step}
           onChange={defineStep}
